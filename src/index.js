5f241aa5d7b24650a2098aa17065a5fa42155765
LineCoran <vppisya@gmail.com>
2022-08-29 20:45:17 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -3,20 +3,40 @@ import cardsDataBrown from '../data/mythicCards/brown';
 import cardsDataBlue from '../data/mythicCards/blue';
 import cardsDataGreen from '../data/mythicCards/green';
 
+let threIsDifficulty = false;
+let threIsAnctient = false;
 let difficulty = '';
 let ancient = '';
+let currentNumberOfCard = 0;
+let globalLinkForCard;
+let currentStages;
+
+let cardsOnFirstStage = 0;
+let cardsOnSecondStage = 0;
+let cardsOnThirdStage = 0;
+let arrCardsAllStages = [];
 
 
+const ancientsCards = document.querySelectorAll('.ancient__card');
 const ancients = document.querySelectorAll('.ancient__card')
 const difficultyButtons = document.querySelectorAll('.difficulty');
 const mixDeckButton = document.querySelector('.mix__button');
+const deckButton = document.querySelector('.deck');
+const lastCard = document.querySelector('.last__card');
 
 
 function listenerForDifficultyButtons() {
     for (let i = 0; i < difficultyButtons.length; i++) {
         difficultyButtons[i].addEventListener('click', function(item) {
+            for (let i = 0; i<difficultyButtons.length; i++) {
+                if (difficultyButtons[i].classList.contains('difficulty-active')) {
+                    difficultyButtons[i].classList.remove('difficulty-active');
+                }
+            }
+            item.target.classList.add('difficulty-active');
             difficulty = item.target.id;
-            console.log(difficulty);
+            threIsDifficulty = true;
+            
         })
     }
 }
@@ -24,21 +44,30 @@ function listenerForDifficultyButtons() {
 function listenerForAnctientCards() {
     for (let i = 0; i < ancients.length; i++) {
         ancients[i].addEventListener('click', function(item) {
+            
+            for (let i = 0; i<ancients.length; i++) {
+                if (ancients[i].classList.contains('ancient__card-active')) {
+                    ancients[i].classList.remove('ancient__card-active');
+                }
+            }
+            item.target.classList.add('ancient__card-active');
             ancient = item.target.id;
-            console.log(ancient);
+            threIsAnctient = true;
         })
     }
 }
 
 function startGlobalCard() {
+    let brownCards = [...cardsDataBrown];
+    let greenCards = [...cardsDataGreen];
+    let blueCards = [...cardsDataBlue];
+
     return  {
-        blueCards: cardsDataBlue,
-        greenCards: cardsDataGreen,
-        brownCards: cardsDataBrown,
+        blueCards: blueCards,
+        greenCards: greenCards,
+        brownCards: brownCards,
     }
 }
-
-const ancientsCards = document.querySelectorAll('.ancient__card');
 
 function setBackgroundImageOnAncient() {
     for (let i = 0; i<ancientsCards.length; i++) {
@@ -60,28 +89,41 @@ function returnRandomNum(num) {
     return (Math.floor(Math.random()*num));
 }
 
-function createMassiveCards(obj, globalCards) {
+function createMassiveCards(obj) {
+
+    currentNumberOfCard = 0;
+
+    let cards = startGlobalCard();
+
     let allStages = {
-        firstStage: obj.firstStage,
-        secondStage: obj.secondStage,
-        thirdStage: obj.thirdStage
+        firstStage: {...obj.firstStage},
+        secondStage: {...obj.secondStage},
+        thirdStage: {...obj.thirdStage}
     }
+
+    currentStages = {
+        ...allStages
+    }
+
+    console.log(allStages);
     let globalArr = [];
     for (let key in allStages) {
         let stage = [];
         for (let prop in allStages[key]) {
             let count = allStages[key][prop];
             while (count != 0) {
-                let randomNum = returnRandomNum(globalCards[prop].length);
-                if (checkCardsSetting(globalCards[prop][randomNum], difficulty, globalCards[prop])) {
-                    stage.push(globalCards[prop][randomNum]);
+                let randomNum = returnRandomNum(cards[prop].length);
+                if (checkCardsSetting(cards[prop][randomNum], difficulty, cards[prop])) {
+                    stage.push(cards[prop][randomNum]);
                     count--;
-                    globalCards[prop].splice(randomNum, 1);
+                    cards[prop].splice(randomNum, 1);
                 }
             }
         }
         globalArr.push(stage);
     }
+
+
     return globalArr;
 }
 
@@ -133,12 +175,118 @@ function init() {
 }
 
 
-
 init();
 
 function createDeck() {
-    let arr = createMassiveCards(findCurrentAncient(), startGlobalCard());
-    console.log(arr);
+    if (!threIsAnctient) {
+        alert('Выберите Древнего');
+        return
+    }
+
+    if (!threIsDifficulty) {
+        alert('Выберите сложность')
+        return;
+    }
+    let arr = createMassiveCards(findCurrentAncient());
+    globalLinkForCard = createLinkForCards(arr);
+
+    deckButton.addEventListener('click', showNextCard);
+    updageCurrentState();
 }
+
+function createLinkForCards(mainArr) {
+    let cardsLink = [];
+
+    console.log(mainArr);
+
+    let cardsLinkWithStages = [];
+
+    for (let i = 0; i < mainArr.length; i++) {
+        let step = [];
+        for (let j = 0; j < mainArr[i].length; j++) {
+            step.push(mainArr[i][j]);
+        } 
+        cardsLinkWithStages.push(step);
+    }
+
+
+    for (let i = 0; i < cardsLinkWithStages.length; i++) {
+        cardsLinkWithStages[i].sort(() => Math.random() - 0.5)
+    }
+
+
+    for (let i = 0; i<cardsLinkWithStages.length; i++) {
+        for (let j = 0; j<cardsLinkWithStages[i].length; j++) {
+            cardsLink.push(cardsLinkWithStages[i][j]);
+        }
+    }
+    console.log(cardsLink);
+    return cardsLink;
+}
+
+function showNextCard() {
+
+    console.log(globalLinkForCard);
+    if (currentNumberOfCard == globalLinkForCard.length) {
+        alert('Колода закончилась');
+        deckButton.removeEventListener('click', showNextCard);
+        return;
+    }
+    lastCard.style.backgroundImage = `url(${globalLinkForCard[currentNumberOfCard].cardFace})`;
+    
+
+    updateValueOnCurrentState(globalLinkForCard[currentNumberOfCard].color);
+    updageCurrentState();
+    console.log(globalLinkForCard[currentNumberOfCard].cardFace);
+    console.log(globalLinkForCard[currentNumberOfCard].color);
+
+    currentNumberOfCard++
+    
+}
+
+function updateValueOnCurrentState(color) {
+    cardsOnFirstStage = sumCardsOnStage(currentStages.firstStage);
+    cardsOnSecondStage = sumCardsOnStage(currentStages.secondStage);
+    cardsOnThirdStage = sumCardsOnStage(currentStages.thirdStage);    
+
+    if (cardsOnFirstStage > 0) {
+            currentStages.firstStage[`${color}Cards`]-=1;
+    } else if (cardsOnSecondStage > 0) {
+        currentStages.secondStage[`${color}Cards`]-=1;
+    } else if (cardsOnThirdStage > 0) {
+        currentStages.thirdStage[`${color}Cards`]-=1;
+    } else {
+        alert('gameove');
+    }
+}
+
+function updageCurrentState() {
+
+    arrCardsAllStages = [];
+
+    for (let key in currentStages) {
+        for (let prop in currentStages[key]) {
+            arrCardsAllStages.push(currentStages[key][prop]);
+        }
+    }
+
+    let dotsInCurrentState = document.querySelectorAll('.dot');
+
+    for (let i = 0; i < dotsInCurrentState.length; i++) {
+        dotsInCurrentState[i].innerHTML = arrCardsAllStages[i];
+    }
+
+}
+
+function sumCardsOnStage(stage) {
+    let sum = 0;
+    for (let key in stage) {
+        sum+=stage[key]
+    }
+    return sum;
+}
+
+
+
 
 mixDeckButton.addEventListener('click', createDeck);
